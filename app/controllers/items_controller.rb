d37e6ef8d47e21693e52333734f3b43b84f79f8b
mknyambura <kariuki.mercie@gmail.com>
2023-03-20 09:42:46 +0300

    completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,32 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(name: params[:name], description: params[:description], price: params[:price])
+    # item = Item.create(name: params[:name], description: params[:description], price: params[:price], user_id: params[:user_id])
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: {error: "Not Found"}, status: :not_found
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,35 @@
 class UsersController < ApplicationController
 
+  # def index
+  #   if params[:id]
+  #     user = User.find(params[:id])
+  #     items = user.items
+  #   else
+  #     items = Item.all
+  #   end
+  #   render json: items, include: :user
+  # end
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+  # def items_index
+  #   user = User.find(params[:id])
+  #   items = user.items
+  #   render json: items, include: :user
+  # end
+
+  # def items
+  #   item = Item.find(params[:id])
+  #   render json: item, include: :user
+  # end
+
+  private
+
+  def render_not_response
+    render json: { error: "Dog house not found" }, status: :not_found
   end
 
 end
